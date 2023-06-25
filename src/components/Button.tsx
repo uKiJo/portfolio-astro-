c853b9f80a8279d289d555015b0e0b124e43e9ec
ukijo <nes-you-ukijo@hotmail.fr>
2023-06-25 01:16:20 +0100

    fix link href

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,7 @@ interface ButtonProps {
 function CustomButton({ children, url }: ButtonProps) {
   return (
     <a
-      href={`/${url}`}
+      href={`${url}`}
       className="inline-block bg-secondary text-primary font-bold md:px-8 py-2 xs:px-6 hover:shadow-card transition rounded"
     >
       {children}
