343ffb78b3652568ca934b3cb694f456e101463a
ukijo <nes-you-ukijo@hotmail.fr>
2023-06-28 17:19:15 +0100

    set an explicit width and height on image

--- src/components/Thumbnails.tsx
+++ src/components/Thumbnails.tsx
@@ -7,7 +7,13 @@ const Thumbnail: React.FC<ThumbnailProps> = ({ title, image }) => {
   return (
     <div className="w-48 h-32 p-4 bg-gradient-180 from-gray-100 to-white dark:bg-transparent dark:bg-gradient-180 dark:from-transparent dark:to-lgrey shadow-card2 hover:shadow-card hover:-translate-y-1 cursor-pointer rounded overflow-hidden flex justify-center transition">
       <a href="/projects">
-        <img className="h-full w-full" src={image} alt={title} />
+        <img
+          className="h-full w-full"
+          src={image}
+          alt={title}
+          width="100%"
+          height="100%"
+        />
       </a>
     </div>
   );
