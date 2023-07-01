1a00a315b537971580e1978bd543a6720bc6cb29
ukijo <nes-you-ukijo@hotmail.fr>
2023-07-01 01:14:33 +0100

    final fixes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -39,9 +39,9 @@ const Card: React.FC<CardProps> = ({
   return (
     <div className="card md:w-64 md:h-96 xs:w-56 bp:p-6 xs:p-4 flex flex-col hover:shadow-card dark:hover:shadow-none rounded-md overflow-hidden bg-grey dark:bg-lprimary cursor-pointer bg-[#fff] transition-shadow">
       <div className="rounded-lg items-center">
-        <div className="p-4 dark:bg-transparent dark:bg-gradient-180 dark:from-transparent dark:to-lgrey bg-gradient-180 from-transparent to-gray-100 rounded-lg dark:border-0 ">
+        <div className="p-4 dark:bg-transparent md:h-[170px] xs:h-[150px] dark:bg-gradient-180 dark:from-transparent dark:to-lgrey bg-gradient-180 from-transparent to-gray-100 rounded-lg dark:border-0 ">
           <img
-            className="h-2/3 rounded "
+            className="rounded "
             src={image}
             alt={title}
             width="100%"
